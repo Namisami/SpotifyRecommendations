bb136b39a179c06337dea1c548000737b255d4e8
mr.sokol5253 <mr.sokol5253@gmail.com>
2022-01-20 00:56:36 +0300

    Переделал бутстрап

--- App/views.py
+++ App/views.py
@@ -16,10 +16,12 @@ def top_genres(request):
     sp = spotify_api.SP()
     login = sp.sp_login()
     top_genres = sp.sp_top_genres()
-    context = {**login, **top_genres}
+    context = {**login, **top_genres, **{'popular_genres':'Популярные жанры:'}}
+    print(context)
     return render(request, 'App/user.html', context)
 
 def create_playlist(request):
     sp = spotify_api.SP()
     sp.create_playlist()
-    return render(request, 'App/user.html')
+    context = sp.sp_login()
+    return render(request, 'App/user.html', context)
